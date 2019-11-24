8a49a436ef479cf73589494cdeff5553651cad60
Ananda Dwi Rahmawati <misskecup.bung@gmail.com>
2019-11-24 22:18:22 +0700

    Create variable_in_string.py

---
+++ variable_in_string.py
@@ -0,0 +1,10 @@
+first_name = "Ananda"
+last_name = "Dwi"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+
+
+#different methode
+first = "Ananda"
+last = "Dwi Rahmawati"
+print(first, last)
