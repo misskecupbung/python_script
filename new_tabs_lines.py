604e954727a1f53879880b0c2062ac69cc4fdc15
Ananda Dwi Rahmawati <misskecup.bung@gmail.com>
2019-11-24 22:32:19 +0700

    Create new_tabs_lines.py

---
+++ new_tabs_lines.py
@@ -0,0 +1,8 @@
+#newline
+print("Nama:\nAnanda\nDwi\nRahmawati")
+
+#newline with tab
+print("Nama:\n\tAnanda\n\tDwi\n\tRahmawati")
+
+#newline with double tab
+print("Nama:\n\t\tAnanda\n\t\tDwi\n\t\tRahmawati")
