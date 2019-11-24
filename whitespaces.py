ad02e69450ee75b634188c6c7a8a656be69e3fe6
Ananda Dwi Rahmawati <misskecup.bung@gmail.com>
2019-11-24 22:49:00 +0700

    Create whitespaces.py

---
+++ whitespaces.py
@@ -0,0 +1,14 @@
+#for right whitespace
+favorite_language = 'python '
+favorite_language = favorite_language.rstrip()
+print(favorite_language)
+
+#for left whitespace
+favorite_language = ' python'
+favorite_language = favorite_language.lstrip()
+print(favorite_language)
+
+#for right and left whitespace
+favorite_language = ' python '
+favorite_language = favorite_language.strip()
+print(favorite_language)
